2b4e5529d54a918be2bbd4b0e06b0d0692d1552f
meraihan <sayedmahmudraihan@gmail.com>
2020-06-17 20:55:54 +0600

    user rowmapper

--- src/main/java/com/matthewgeorgiev/project/MoviePreview/model/rowmapper/UserRowMapper.java
+++ src/main/java/com/matthewgeorgiev/project/MoviePreview/model/rowmapper/UserRowMapper.java
@@ -1,6 +1,7 @@
 package com.matthewgeorgiev.project.MoviePreview.model.rowmapper;
 
 import com.matthewgeorgiev.project.MoviePreview.model.User;
+import com.matthewgeorgiev.project.MoviePreview.utils.Helper;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -10,12 +11,13 @@ public class UserRowMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet resultSet, int i) throws SQLException {
         User user = new User();
-        user.setUsername(resultSet.getString("username"));
-        user.setPassword(resultSet.getString("password"));
-        user.setName(resultSet.getString("name"));
-        user.setPhone(resultSet.getString("phone"));
         user.setId(resultSet.getInt("id"));
+        user.setUsername(resultSet.getString("username"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
         user.setRole(resultSet.getString("role"));
+        user.setLastLoginAt(Helper.timeStampToLocalDateTime(resultSet.getTimestamp("last_login_at")));
+        user.setCreatedAt(Helper.timeStampToLocalDateTime(resultSet.getTimestamp("created_at")));
         return user;
     }
 }
