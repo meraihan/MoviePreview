93ab7a6b78c9f8a3136c13fda73a89c6c2104d5c
meraihan <sayedmahmudraihan@gmail.com>
2020-06-18 18:24:02 +0600

    rating desc done and slider not fully done yet

--- src/main/java/com/matthewgeorgiev/project/MoviePreview/model/Rating.java
+++ src/main/java/com/matthewgeorgiev/project/MoviePreview/model/Rating.java
@@ -2,10 +2,13 @@ package com.matthewgeorgiev.project.MoviePreview.model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class Rating {
     public Integer id;
     public User user;
     public String imdbID;
     public String rating;
+    public LocalDateTime createdAt;
 }
